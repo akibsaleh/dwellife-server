b340bad4c43950c646c74844741e645552229b50
Akib Saleh <akibzihan@gmail.com>
2023-11-24 11:27:37 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+
+app.get('/api', (req, res) => {
+  res.send('Hello World!');
+});
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
