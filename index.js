c0a23247eb9907c1e8568a2473aabaaaa8a5c9fc
Akib Saleh <akibzihan@gmail.com>
2023-11-24 19:10:29 +0600

    Connection to mongodb established

--- index.js
+++ index.js
@@ -6,6 +6,31 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
+// MiddleWares
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hfh6rjb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 app.get('/api', (req, res) => {
   res.send('Hello World!');
 });
